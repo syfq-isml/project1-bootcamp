b2d56fbd5bfa3869c86089b415205e2ae629a05f
Syafiq Ismail <macamyesjer@gmail.com>
2023-04-12 12:40:50 +0800

    style homepage logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,8 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import brainImg from "../assets/images/brain_animated.png";
+import brainBrawn from "../assets/images/Brainbrawn_circle.png";
+
 import { getRandomIntInclusive } from "../utils";
 
 const LOCALSTORAGE_KEY_HISCORE = "hiScores";
@@ -80,12 +82,17 @@ class Home extends Component {
                             spacing={2}
                         >
                             <Paper
-                                className="fl-row fl-centered"
+                                className="fl-row fl-centered logo-spinner"
                                 style={{
                                     borderRadius: "50%",
                                     width: "20vw",
                                     height: "auto",
                                     aspectRatio: "1/1",
+                                    backgroundImage: `url(${brainBrawn})`,
+                                    backgroundSize: "32vw",
+                                    backgroundPosition: "center",
+                                    backgroundRepeat: "no-repeat",
+                                    backgroundColor: "#fdf7ea",
                                 }}
                                 elevation={0}
                             >
@@ -93,16 +100,20 @@ class Home extends Component {
                                     className="fl-row fl-centered"
                                     style={{
                                         borderRadius: "50%",
-                                        width: "70%",
+                                        width: "60%",
                                         height: "auto",
                                         aspectRatio: "1/1",
+                                        backgroundColor: "#fdf7ea",
                                     }}
                                     elevation={0}
                                 >
                                     <img
+                                        className="logo-spinner__child"
                                         src={brainImg}
                                         alt="animated brain"
-                                        style={{ width: "100%" }}
+                                        style={{
+                                            width: "100%",
+                                        }}
                                     />
                                 </Paper>
                             </Paper>
